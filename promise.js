8005f189d0fcfe95c884a85886a85b7af421f75b
Yurij <yura.malahov@gmail.com>
2015-12-07 23:10:48 +0200

    Update promise.js

--- promise.js
+++ promise.js
@@ -16,13 +16,15 @@ Promise.resolve(requestPromise(1.1))
 
 //***
 
-var p2 =requestPromise(2.1)
+var p2 = requestPromise(2.1)
   .then(function (resultOfRequest) {
     console.log(resultOfRequest);
+    return resultOfRequest;
   })
   .catch(function (error) {
     console.log('error2');
   });
+
 
 //***
 
@@ -33,6 +35,7 @@ var p3 = requestPromise(3.1)
   })
   .then(function (resultOfRequest2) {
     console.log(resultOfRequest2);
+    return resultOfRequest2;
   })
   .catch(function (error) {
     console.log('error3');
@@ -43,6 +46,7 @@ var p3 = requestPromise(3.1)
 var p4 = Promise.all([requestPromise(4.1), requestPromise(4.2)])
   .then(function (result) {
     console.log(result);
+    return result;
   })
   .catch(function (error) {
     console.log('error4');
@@ -53,6 +57,7 @@ var p4 = Promise.all([requestPromise(4.1), requestPromise(4.2)])
 var p5 = Promise.race([requestPromise(5.1), requestPromise(5.2)])
   .then(function (result) {
     console.log(result);
+    return result;
   })
   .catch(function (error) {
     console.log('error5');
@@ -62,9 +67,10 @@ var p5 = Promise.race([requestPromise(5.1), requestPromise(5.2)])
 
 var p6 = requestPromise(6.1)
   .then(function (result1) {
-    requestPromise(6.2)
+    return requestPromise(6.2)
       .then(function (result2) {
         console.log(result1, result2);
+        return [result1, result2];
       });
   })
   .catch(function (error) {
