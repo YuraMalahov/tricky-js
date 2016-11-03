7357f2ddc33e9ee1c8a6d690bdf515b1e380c93a
Yuri <iurii.malakhovskyi@globallogic.com>
2016-11-04 00:28:37 +0200

    Create async-await.js

---
+++ async-await.js
@@ -0,0 +1,39 @@
+"use strict";
+
+async function test() {
+    console.log("start");
+    
+    var a = await request(1000);
+    console.log(a);
+    
+    fetch().then((result) => console.log(result));
+    
+    console.log("end");
+    
+    return "finish";
+}
+
+async function request(time) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve("hi!");
+        }, time)
+    });
+}
+
+async function fetch() {
+    return "world";
+}
+
+console.log("1");
+test().then((result) => console.log(result));
+console.log("2");
+
+
+Promise
+    .all([test(), test(), test()])
+    .then(results => console.log(results));
+
+test().then(result => console.log("1: ", result));
+test().then(result => console.log("2: ", result));
+test().then(result => console.log("3: ", result));
